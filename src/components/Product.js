75be7b38fd2e0900302784a5ee0739077c064d13
sanketwakhare <43931717+sanketwakhare@users.noreply.github.com>
2023-07-01 18:15:35 +0530

    add product details

--- src/components/Product.js
+++ src/components/Product.js
@@ -6,7 +6,7 @@ const Product = (props) => {
   return (
     <Link to={`/products/${product.id}`} className="product">
       <img src={product.image}></img>
-      <div className="prouct-details-container">
+      <div className="prouct-info">
         <div className="product-title">{product.title}</div>
         <div className="product-price">$ {product.price}</div>
       </div>
--- src/components/ProductDetails.js
+++ src/components/ProductDetails.js
@@ -1,14 +1,56 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 import useApi from '../hooks/useApi';
+import ProductDetailsLoader from '../components/Loader/ProductDetailsLoader';
+
+import './product-details.scss';
 
 const ProductDetails = () => {
   const { productId } = useParams();
-  const { data: product } = useApi({
+  const {
+    data: product,
+    loading,
+    loadError,
+  } = useApi({
     url: `https://fakestoreapi.com/products/${productId}`,
   });
 
-  return <div>Product Details Page {product.title}</div>;
+  if (loading === true)
+    return (
+      <div className="center">
+        <ProductDetailsLoader cardCount={1} />
+      </div>
+    );
+
+  return (
+    <div className="product-details-container">
+      <div className="product-details">
+        <img src={product.image} alt="product"></img>
+        <div className="product-details__container">
+          <div className="product-details__title">
+            <div>{product.title}</div>
+          </div>
+          <div className="product-details__price">
+            {/* <label>Price:</label> */}
+            <span>{product.price}</span>
+          </div>
+          <div className="product-details__description">
+            <label>Description:</label>
+            <p>{product.description}</p>
+          </div>
+          <div className="product-details__category">
+            <label>Category:</label>
+            <span>{product.category}</span>
+          </div>
+          <div className="product-details__ratings">
+            <label>Ratings:</label>
+            <span>{product.rating?.rate}</span>
+            {/* <span>{product.rating?.count}</span> */}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ProductDetails;
