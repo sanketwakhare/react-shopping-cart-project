84a9e5d885290eb8411937b083c643bfc5fad64d
sanketwakhare <43931717+sanketwakhare@users.noreply.github.com>
2023-07-05 23:59:01 +0530

    2 files modified

--- src/components/AddToCart/AddToCart.js
+++ src/components/AddToCart/AddToCart.js
@@ -1,23 +1,30 @@
-import { useCartContext } from "../../context/CartContext";
+import React from 'react';
+import { useCartContext } from '../../context/CartContext';
 import './add-to-cart.scss';
 
 const AddToCart = (props) => {
+  const { product } = props;
+  const { cartItems, addItemToCart, removeItemFromCart } = useCartContext();
+  const isProductPresentInCart = cartItems.find(
+    (item) => item.product.id === product.id
+  );
 
-    const { product } = props;
-    const { cartItems, addItemToCart, removeItemFromCart } = useCartContext();
-    const isProductPresentInCart = cartItems.find(item => item.product.id === product.id);
-
-    return <div className="add-to-cart-container">
-        {isProductPresentInCart &&
-            <>
-                <button onClick={() => removeItemFromCart(product)}>-</button>
-                <span className="spacing__quantity">{isProductPresentInCart.quantity}</span>
-                <button onClick={() => addItemToCart(product)}>+</button>
-            </>}
-        {!isProductPresentInCart &&
-            <button onClick={() => addItemToCart(product)}>Add To Cart</button>
-        }
+  return (
+    <div className="add-to-cart-container">
+      {isProductPresentInCart && (
+        <>
+          <button onClick={() => removeItemFromCart(product)}>-</button>
+          <span className="spacing__quantity">
+            {isProductPresentInCart.quantity}
+          </span>
+          <button onClick={() => addItemToCart(product)}>+</button>
+        </>
+      )}
+      {!isProductPresentInCart && (
+        <button onClick={() => addItemToCart(product)}>Add To Cart</button>
+      )}
     </div>
+  );
 };
 
 export default AddToCart;
--- src/context/CartContext.js
+++ src/context/CartContext.js
@@ -1,52 +1,61 @@
-import { createContext, useContext, useState } from 'react';
+import React, { createContext, useContext, useState } from 'react';
 
 const CartContext = createContext();
 
 export const useCartContext = () => {
-    return useContext(CartContext);
+  return useContext(CartContext);
 };
 
 const initialCartState = [];
 
 const CartContextProvider = ({ children }) => {
+  const [cartItems, setCartItems] = useState(initialCartState);
 
-    const [cartItems, setCartItems] = useState(initialCartState);
+  const addItemToCart = (product) => {
+    setCartItems((prevCartItems) => {
+      let newCartItems = [].concat(prevCartItems);
+      let existingItem = newCartItems.find(
+        (item) => item.product.id === product.id
+      );
+      if (existingItem) {
+        existingItem.quantity++;
+      } else {
+        newCartItems.push({ product: product, quantity: 1 });
+      }
+      return newCartItems;
+    });
+  };
 
-    const addItemToCart = (product) => {
-        setCartItems((prevCartItems) => {
-            let newCartItems = [].concat(prevCartItems);
-            let existingItem = newCartItems.find(item => item.product.id === product.id);
-            if (existingItem) {
-                existingItem.quantity++;
-            } else {
-                newCartItems.push({ product: product, quantity: 1 });
-            }
-            return newCartItems;
-        })
-    };
+  const removeItemFromCart = (product) => {
+    setCartItems((prevCartItems) => {
+      let newCartItems = [].concat(prevCartItems);
+      let existingItem = newCartItems.find(
+        (item) => item.product.id === product.id
+      );
+      if (existingItem) {
+        if (existingItem.quantity > 1) {
+          existingItem.quantity--;
+        } else {
+          newCartItems = newCartItems.filter(
+            (item) => item.product.id !== product.id
+          );
+        }
+      }
+      return newCartItems;
+    });
+  };
 
-    const removeItemFromCart = (product) => {
-        setCartItems((prevCartItems) => {
-            let newCartItems = [].concat(prevCartItems);
-            let existingItem = newCartItems.find(item => item.product.id === product.id);
-            if (existingItem) {
-                if (existingItem.quantity > 1) {
-                    existingItem.quantity--;
-                } else {
-                    newCartItems = newCartItems.filter(item => item.product.id !== product.id);
-                }
-            }
-            return newCartItems;
-        });
-    };
+  const clearCart = () => {
+    setCartItems(() => initialCartState);
+  };
 
-    const clearCart = () => {
-        setCartItems(() => initialCartState);
-    };
-
-    return <CartContext.Provider value={{ cartItems, addItemToCart, removeItemFromCart, clearCart }}>
-        {children}
-    </CartContext.Provider>;
+  return (
+    <CartContext.Provider
+      value={{ cartItems, addItemToCart, removeItemFromCart, clearCart }}
+    >
+      {children}
+    </CartContext.Provider>
+  );
 };
 
 export default CartContextProvider;
