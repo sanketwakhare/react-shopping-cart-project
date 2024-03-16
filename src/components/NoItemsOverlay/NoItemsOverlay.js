37c06af7b240b2623981f15bbed13cf73c2fa099
sanketwakhare <sanketwakhare@gmail.com>
2024-03-16 16:35:03 +0530

    fix dummy components no items and page not found

--- src/components/NoItemsOverlay/NoItemsOverlay.js
+++ src/components/NoItemsOverlay/NoItemsOverlay.js
@@ -2,7 +2,12 @@ import React from "react";
 import "./no-items-overlay.scss";
 
 const NoItemsOverlay = () => {
-  return <div className="no-items-container">Page Not Found</div>;
+  return (
+    <div className="no-items-container">
+      <div>It seems there are no products that match your criteria.</div>
+      <div>Feel free to explore other categories.</div>
+    </div>
+  );
 };
 
 export default NoItemsOverlay;
---
+++ src/components/PageNotFound/PageNotFound.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./page-not-found.scss";
+
+const PageNotFound = () => {
+  return (
+    <div className="page-not-found-container">
+      <div>Oops! we can't seem to find the page you're looking for.</div>
+      <div>Feel free to browse other parts of our site.</div>
+    </div>
+  );
+};
+
+export default PageNotFound;
