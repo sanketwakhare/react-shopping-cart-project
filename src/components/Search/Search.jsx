003ea951f866ad5a7f274177bac2968b372ef0ae
sanketwakhare <sanketwakhare@gmail.com>
2024-03-23 00:15:46 +0530

    add clear for search input

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router";
 import "./search.scss";
 
 const Search = () => {
   const navigate = useNavigate();
+  const [freeText, setFreeText] = useState("");
 
   const handleSubmit = async (event) => {
     event.preventDefault();
@@ -12,9 +13,40 @@ const Search = () => {
     navigate("search", { state: { searchString } });
   };
 
+  const handleClear = async (event) => {
+    event.preventDefault();
+    setFreeText("");
+  };
+
+  const handleOnChange = async (event) => {
+    event.preventDefault();
+    setFreeText(event?.target?.value?.trim());
+  };
+
+  const clearSearchClasses = ["clear-search"];
+  if (freeText) {
+    clearSearchClasses.push("show-clear-search");
+  }
+
   return (
     <form onSubmit={handleSubmit} className="search-container">
-      <input type="text" name="global-search" placeholder="search products" />
+      <div>
+        <input
+          type="text"
+          name="global-search"
+          placeholder="search products"
+          className="search-input"
+          value={freeText}
+          onChange={handleOnChange}
+        />
+        <button
+          type="button"
+          onClick={handleClear}
+          className={clearSearchClasses.join(" ")}
+        >
+          x
+        </button>
+      </div>
       <input type="submit" value="Search"></input>
     </form>
   );
