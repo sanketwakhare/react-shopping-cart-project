b1c06b5cb59073a8f3d3e8c60c743f672f526b4f
sanketwakhare <sanketwakhare@gmail.com>
2024-03-25 08:37:53 +0530

    use gravatar for random profile avatar

---
+++ src/components/UserProfile/ProfileAvatar.jsx
@@ -0,0 +1,10 @@
+import gravatar from "gravatar";
+
+const ProfileAvatar = ({ email }) => {
+  // Generate Gravatar URL based on the email address
+  const avatarUrl = gravatar.url(email, { s: "200", d: "identicon", r: "pg" });
+
+  return <img src={avatarUrl} alt="Avatar" />;
+};
+
+export default ProfileAvatar;
